4d7d48ce75f8769be930450721ba52611faf369f
Zahid Hasan Milu <g.one.milu@gmail.com>
2022-06-07 05:31:44 +0600

    Employee Datadata Model

--- EmpApp/models.py
+++ EmpApp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Roll(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    eid = models.IntegerField(unique=True, primary_key=True, auto_created=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    dept = models.ForeignKey(Department,  on_delete=models.CASCADE)
+    salary = models.IntegerField(default=0)
+    bonus = models.IntegerField(default=0)
+    roll = models.ForeignKey(Roll, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=11)
+    hire_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name
